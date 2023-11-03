5714f15292dc9568057929027c32ea67f4c29273
Brian Sam-Bodden <bsb@redis.com>
2023-11-03 15:10:35 -0700

    Add connections to LandmarksPlayer

--- LandmarksPlayer.py
+++ LandmarksPlayer.py
@@ -24,28 +24,275 @@ print(df.columns)
 
 
 class VideoPlayer(tk.Toplevel):
-    HAND_CONNECTIONS = frozenset([
-        # Palm
-        (0, 1), (0, 5), (9, 13), (13, 17), (5, 9), (0, 17),
-        # Thumb
-        (1, 2), (2, 3), (3, 4),
-        # Index finger
-        (5, 6), (6, 7), (7, 8),
-        # Middle finger
-        (9, 10), (10, 11), (11, 12),
-        # Ring finger
-        (13, 14), (14, 15), (15, 16),
-        # Pinky
-        (17, 18), (18, 19), (19, 20),
-    ])
+    HAND_CONNECTIONS = frozenset(
+        [
+            # Palm
+            (0, 1),
+            (0, 5),
+            (9, 13),
+            (13, 17),
+            (5, 9),
+            (0, 17),
+            # Thumb
+            (1, 2),
+            (2, 3),
+            (3, 4),
+            # Index finger
+            (5, 6),
+            (6, 7),
+            (7, 8),
+            # Middle finger
+            (9, 10),
+            (10, 11),
+            (11, 12),
+            # Ring finger
+            (13, 14),
+            (14, 15),
+            (15, 16),
+            # Pinky
+            (17, 18),
+            (18, 19),
+            (19, 20),
+        ]
+    )
 
-    POSE_CONNECTIONS = frozenset([(0, 1), (1, 2), (2, 3), (3, 7), (0, 4), (4, 5),
-                                  (5, 6), (6, 8), (9, 10), (11, 12), (11, 13),
-                                  (13, 15), (15, 17), (15, 19), (15, 21), (17, 19),
-                                  (12, 14), (14, 16), (16, 18), (16, 20), (16, 22),
-                                  (18, 20), (11, 23), (12, 24), (23, 24), (23, 25),
-                                  (24, 26), (25, 27), (26, 28), (27, 29), (28, 30),
-                                  (29, 31), (30, 32), (27, 31), (28, 32)])
+    POSE_CONNECTIONS = frozenset(
+        [
+            # Updated pose connections excluding hands and face
+            (0, 1),
+            (1, 2),
+            (2, 3),
+            (3, 7),
+            (0, 4),
+            (4, 5),
+            (5, 6),
+            (6, 8),
+            (9, 10),
+            (11, 12),
+            (11, 13),
+            (13, 15),
+            (15, 17),
+            (12, 14),
+            (14, 16),
+            (16, 18),
+            (11, 23),
+            (12, 24),
+            (23, 24),
+        ]
+    )
+
+    # FACEMESH
+
+    FACEMESH_LIPS = frozenset(
+        [
+            (61, 146),
+            (146, 91),
+            (91, 181),
+            (181, 84),
+            (84, 17),
+            (17, 314),
+            (314, 405),
+            (405, 321),
+            (321, 375),
+            (375, 291),
+            (61, 185),
+            (185, 40),
+            (40, 39),
+            (39, 37),
+            (37, 0),
+            (0, 267),
+            (267, 269),
+            (269, 270),
+            (270, 409),
+            (409, 291),
+            (78, 95),
+            (95, 88),
+            (88, 178),
+            (178, 87),
+            (87, 14),
+            (14, 317),
+            (317, 402),
+            (402, 318),
+            (318, 324),
+            (324, 308),
+            (78, 191),
+            (191, 80),
+            (80, 81),
+            (81, 82),
+            (82, 13),
+            (13, 312),
+            (312, 311),
+            (311, 310),
+            (310, 415),
+            (415, 308),
+        ]
+    )
+
+    FACEMESH_LEFT_EYE = frozenset(
+        [
+            (263, 249),
+            (249, 390),
+            (390, 373),
+            (373, 374),
+            (374, 380),
+            (380, 381),
+            (381, 382),
+            (382, 362),
+            (263, 466),
+            (466, 388),
+            (388, 387),
+            (387, 386),
+            (386, 385),
+            (385, 384),
+            (384, 398),
+            (398, 362),
+        ]
+    )
+
+    FACEMESH_LEFT_IRIS = frozenset([(474, 475), (475, 476), (476, 477), (477, 474)])
+
+    FACEMESH_LEFT_EYEBROW = frozenset(
+        [
+            (276, 283),
+            (283, 282),
+            (282, 295),
+            (295, 285),
+            (300, 293),
+            (293, 334),
+            (334, 296),
+            (296, 336),
+        ]
+    )
+
+    FACEMESH_RIGHT_EYE = frozenset(
+        [
+            (33, 7),
+            (7, 163),
+            (163, 144),
+            (144, 145),
+            (145, 153),
+            (153, 154),
+            (154, 155),
+            (155, 133),
+            (33, 246),
+            (246, 161),
+            (161, 160),
+            (160, 159),
+            (159, 158),
+            (158, 157),
+            (157, 173),
+            (173, 133),
+        ]
+    )
+
+    FACEMESH_RIGHT_EYEBROW = frozenset(
+        [
+            (46, 53),
+            (53, 52),
+            (52, 65),
+            (65, 55),
+            (70, 63),
+            (63, 105),
+            (105, 66),
+            (66, 107),
+        ]
+    )
+
+    FACEMESH_RIGHT_IRIS = frozenset([(469, 470), (470, 471), (471, 472), (472, 469)])
+
+    FACEMESH_FACE_OVAL = frozenset(
+        [
+            (10, 338),
+            (338, 297),
+            (297, 332),
+            (332, 284),
+            (284, 251),
+            (251, 389),
+            (389, 356),
+            (356, 454),
+            (454, 323),
+            (323, 361),
+            (361, 288),
+            (288, 397),
+            (397, 365),
+            (365, 379),
+            (379, 378),
+            (378, 400),
+            (400, 377),
+            (377, 152),
+            (152, 148),
+            (148, 176),
+            (176, 149),
+            (149, 150),
+            (150, 136),
+            (136, 172),
+            (172, 58),
+            (58, 132),
+            (132, 93),
+            (93, 234),
+            (234, 127),
+            (127, 162),
+            (162, 21),
+            (21, 54),
+            (54, 103),
+            (103, 67),
+            (67, 109),
+            (109, 10),
+        ]
+    )
+
+    FACEMESH_NOSE = frozenset(
+        [
+            (168, 6),
+            (6, 197),
+            (197, 195),
+            (195, 5),
+            (5, 4),
+            (4, 1),
+            (1, 19),
+            (19, 94),
+            (94, 2),
+            (98, 97),
+            (97, 2),
+            (2, 326),
+            (326, 327),
+            (327, 294),
+            (294, 278),
+            (278, 344),
+            (344, 440),
+            (440, 275),
+            (275, 4),
+            (4, 45),
+            (45, 220),
+            (220, 115),
+            (115, 48),
+            (48, 64),
+            (64, 98),
+        ]
+    )
+
+    FACEMESH_CONTOURS = frozenset().union(
+        *[
+            FACEMESH_LIPS,
+            FACEMESH_LEFT_EYE,
+            FACEMESH_LEFT_EYEBROW,
+            FACEMESH_RIGHT_EYE,
+            FACEMESH_RIGHT_EYEBROW,
+            FACEMESH_FACE_OVAL,
+        ]
+    )
+
+    FACEMESH_IRISES = frozenset().union(*[FACEMESH_LEFT_IRIS, FACEMESH_RIGHT_IRIS])
+
+    FACE_CONNECTIONS = frozenset().union(
+        FACEMESH_LIPS,
+        FACEMESH_LEFT_EYE,
+        FACEMESH_RIGHT_EYE,
+        FACEMESH_FACE_OVAL,
+        FACEMESH_LEFT_EYEBROW,
+        FACEMESH_RIGHT_EYEBROW
+        # If you want to include the irises, add FACEMESH_IRISES
+    )
 
     def __init__(self, master, file_path, total_frames):
         super().__init__(master)
@@ -121,24 +368,76 @@ class VideoPlayer(tk.Toplevel):
             self.after(33, self.play_frames)  # Adjusted delay for 30 fps
 
     def draw_hand_connections(self, frame, hand_landmarks, frame_width, frame_height):
-        for (start, end) in self.HAND_CONNECTIONS:
+        for start, end in self.HAND_CONNECTIONS:
             if start < len(hand_landmarks) and end < len(hand_landmarks):
                 start_landmark = hand_landmarks.iloc[start]
                 end_landmark = hand_landmarks.iloc[end]
-                if not pd.isna(start_landmark["x"]) and not pd.isna(start_landmark["y"]) and not pd.isna(end_landmark["x"]) and not pd.isna(end_landmark["y"]):
-                    start_point = (int(start_landmark["x"] * frame_width), int(start_landmark["y"] * frame_height))
-                    end_point = (int(end_landmark["x"] * frame_width), int(end_landmark["y"] * frame_height))
+                if (
+                    not pd.isna(start_landmark["x"])
+                    and not pd.isna(start_landmark["y"])
+                    and not pd.isna(end_landmark["x"])
+                    and not pd.isna(end_landmark["y"])
+                ):
+                    start_point = (
+                        int(start_landmark["x"] * frame_width),
+                        int(start_landmark["y"] * frame_height),
+                    )
+                    end_point = (
+                        int(end_landmark["x"] * frame_width),
+                        int(end_landmark["y"] * frame_height),
+                    )
                     cv2.line(frame, start_point, end_point, (255, 0, 0), 1)
 
     def draw_pose_connections(self, frame, pose_landmarks, frame_width, frame_height):
-        for (start, end) in self.POSE_CONNECTIONS:
+        # Define the indices for the face landmarks in the pose model that you want to exclude
+        face_landmarks_indices = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}  # Add the correct indices here
+
+        for start, end in self.POSE_CONNECTIONS:
+            # Skip connections if either end is a face landmark index
+            if start in face_landmarks_indices or end in face_landmarks_indices:
+                continue
+
             if start < len(pose_landmarks) and end < len(pose_landmarks):
                 start_landmark = pose_landmarks.iloc[start]
                 end_landmark = pose_landmarks.iloc[end]
-                if not pd.isna(start_landmark["x"]) and not pd.isna(start_landmark["y"]) and not pd.isna(end_landmark["x"]) and not pd.isna(end_landmark["y"]):
-                    start_point = (int(start_landmark["x"] * frame_width), int(start_landmark["y"] * frame_height))
-                    end_point = (int(end_landmark["x"] * frame_width), int(end_landmark["y"] * frame_height))
+                if (
+                    not pd.isna(start_landmark["x"])
+                    and not pd.isna(start_landmark["y"])
+                    and not pd.isna(end_landmark["x"])
+                    and not pd.isna(end_landmark["y"])
+                ):
+                    start_point = (
+                        int(start_landmark["x"] * frame_width),
+                        int(start_landmark["y"] * frame_height),
+                    )
+                    end_point = (
+                        int(end_landmark["x"] * frame_width),
+                        int(end_landmark["y"] * frame_height),
+                    )
                     cv2.line(frame, start_point, end_point, (255, 0, 0), 1)
+
+    def draw_face_connections(self, frame, face_landmarks, frame_width, frame_height):
+        for start, end in self.FACE_CONNECTIONS:
+            if start < len(face_landmarks) and end < len(face_landmarks):
+                start_landmark = face_landmarks.iloc[start]
+                end_landmark = face_landmarks.iloc[end]
+                if (
+                    not pd.isna(start_landmark["x"])
+                    and not pd.isna(start_landmark["y"])
+                    and not pd.isna(end_landmark["x"])
+                    and not pd.isna(end_landmark["y"])
+                ):
+                    start_point = (
+                        int(start_landmark["x"] * frame_width),
+                        int(start_landmark["y"] * frame_height),
+                    )
+                    end_point = (
+                        int(end_landmark["x"] * frame_width),
+                        int(end_landmark["y"] * frame_height),
+                    )
+                    cv2.line(
+                        frame, start_point, end_point, (0, 255, 0), 1
+                    )  # Color for face connections
 
     def display_frame(self):
         frame_width, frame_height = 500, 500
@@ -165,11 +464,22 @@ class VideoPlayer(tk.Toplevel):
                     continue
 
                 if self.show_connections_var.get():
-                    self.draw_hand_connections(frame, hand_landmarks, frame_width, frame_height)
+                    self.draw_hand_connections(
+                        frame, hand_landmarks, frame_width, frame_height
+                    )
 
             pose_landmarks = frame_landmarks[frame_landmarks["type"] == "pose"]
             if not pose_landmarks.empty:
-                self.draw_pose_connections(frame, pose_landmarks, frame_width, frame_height)
+                self.draw_pose_connections(
+                    frame, pose_landmarks, frame_width, frame_height
+                )
+
+            # Draw connections for face landmarks if checkbox is checked
+            face_landmarks = frame_landmarks[frame_landmarks["type"] == "face"]
+            if not face_landmarks.empty:
+                self.draw_face_connections(
+                    frame, face_landmarks, frame_width, frame_height
+                )
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame = cv2.resize(frame, (frame_width, frame_height))
