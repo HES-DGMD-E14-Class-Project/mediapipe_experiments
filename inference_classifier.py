00742eaa2ab0e8099a63266e2fd31b53dd065af9
Wilson Guo <wig979@g.harvard.edu>
2023-12-12 11:53:13 -0500

    Adding Random Forest Model

---
+++ inference_classifier.py
@@ -0,0 +1,80 @@
+import pickle
+
+import cv2
+import mediapipe as mp
+import numpy as np
+
+model_dict = pickle.load(open('./model.p', 'rb'))
+model = model_dict['model']
+
+#Might need to change depending on abailable hardware
+cap = cv2.VideoCapture(1)
+
+#Create the mediapipe objects
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+
+#dictionary containing all the american sign language characters
+labels_dict = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8: 'i', 9: 'j', 10: 'k', 11: 'l', 12: 'm', 13: 'n', 14: 'o', 15: 'p', 16: 'q', 17: 'r', 18: 's', 19: 't', 20: 'u', 21: 'v', 22: 'w', 23: 'x', 24: 'y', 25: 'z'}
+
+    
+while True:
+
+    data_aux = []
+    x_list = []
+    y_list = []
+
+    ret, frame = cap.read()
+
+    H, W, _ = frame.shape
+
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(frame_rgb)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                frame,  # image to draw
+                hand_landmarks,  # model output
+                mp_hands.HAND_CONNECTIONS,  # hand connections
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+
+        for hand_landmarks in results.multi_hand_landmarks:
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+
+                x_list.append(x)
+                y_list.append(y)
+
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+                data_aux.append(x - min(x_list))
+                data_aux.append(y - min(y_list))
+
+        x1 = int(min(x_list) * W) - 10
+        y1 = int(min(y_list) * H) - 10
+
+        x2 = int(max(x_list) * W) - 10
+        y2 = int(max(y_list) * H) - 10
+
+        prediction = model.predict([np.asarray(data_aux)[:42]])
+
+        predicted_character = labels_dict[int(prediction[0])]
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
+        cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 6,
+                    cv2.LINE_AA)
+
+    cv2.imshow('frame', frame)
+    cv2.waitKey(1)
+
+
+cap.release()
+cv2.destroyAllWindows()
